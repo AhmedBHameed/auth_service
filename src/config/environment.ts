c50dffb2eb2a7322fb7657f35a622ac33bc03fc3
AhmedBHameed <ahmedbazy@gmail.com>
2021-05-30 21:26:09 +0200

    Enhance deployment configuration

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,42 +1,21 @@
-import {isDockerized} from 'src/util/isDockerized';
+const {BUILD_ENV, MONGODB_PASS, VERSION, SECRET} = process.env;
 
-const {
-  PORT,
-  // IS_APP_CONTAINERIZED,
-  MONGODB_SERVER,
-  MONGODB_SERVER_NO_CONTAINER,
-  MONGODB_DB,
-  MONGODB_PASS,
-  MONGODB_PORT,
-  MONGODB_USER,
-  LOG_DIR,
-  LOG_LEVEL,
-  NODE_ENV,
-  SECRET,
-  VERSION,
-} = process.env;
-
-const serverHost = isDockerized() ? MONGODB_SERVER : MONGODB_SERVER_NO_CONTAINER;
+const isProd = BUILD_ENV === 'prod';
 
 export default {
   version: VERSION,
-  isProd: NODE_ENV === 'production',
+  isProd: isProd,
   database: {
-    dbName: MONGODB_DB || '',
+    dbName: 'admin',
     password: MONGODB_PASS || '',
-    port: parseInt(MONGODB_PORT || '27017', 10),
-    server: serverHost,
-    user: MONGODB_USER || '',
+    port: 27017,
+    server: 'mango',
+    user: 'super',
   },
   logs: {
-    dir: LOG_DIR || 'logs',
-    level: LOG_LEVEL || 'silly',
+    dir: 'logs',
+    level: isProd ? 'error' : 'debug',
   },
-  port: PORT || '5001',
-  // redis: {
-  //   host: get(process.env, 'REDIS_DOMAIN', '127.0.0.1'),
-  //   pass: get(process.env, 'REDIS_PASSWORD', ''),
-  //   port: parseInt(process.env.REDIS_PORT || '6379'),
-  // },
+  port: '5001',
   secret: SECRET || '',
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,75 @@
+const path = require('path');
+const {DefinePlugin} = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const APP_DIR = path.resolve(__dirname, './src');
+const BUILD_DIR = path.resolve(__dirname, './dist');
+
+const {BUILD_ENV, VERSION, MONGODB_PASS, SECRET} = process.env;
+const isProd = BUILD_ENV === 'production';
+
 function buildConfig() {
-  const {NODE_ENV} = process.env;
-  if (NODE_ENV !== 'development' && NODE_ENV !== 'production')
+  const {BUILD_ENV} = process.env;
+  if (BUILD_ENV !== 'development' && BUILD_ENV !== 'production')
     throw new Error("Wrong webpack build parameter. Possible choices: 'development' or 'production'.");
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const config = require('./config/webpack/' + NODE_ENV + '.js');
-  return config();
+
+  return {
+    entry: {
+      app: `${APP_DIR}/app.ts`,
+    },
+    output: {
+      path: BUILD_DIR,
+      filename: `[name].${BUILD_ENV}.js`,
+    },
+    devtool: isProd ? undefined : 'eval-source-map',
+    mode: isProd ? 'production' : 'development',
+    target: 'node',
+    externals: [nodeExternals()],
+    resolve: {
+      extensions: ['.ts', '.js'],
+      alias: {
+        src: APP_DIR,
+      },
+    },
+    plugins: [
+      new ESLintPlugin(),
+      new DefinePlugin({
+        'process.env': JSON.stringify({
+          BUILD_ENV,
+          VERSION,
+          MONGODB_PASS,
+          SECRET,
+        }),
+      }),
+      new CleanWebpackPlugin(),
+      new NodemonPlugin(),
+    ],
+    module: {
+      rules: [
+        // {
+        //   test: /\.ts$/,
+        //   enforce: 'pre',
+        //   use: [
+        //     {
+        //       options: {
+        //         eslintPath: require.resolve('eslint'),
+        //       },
+        //       loader: require.resolve('eslint-loader'),
+        //     },
+        //   ],
+        //   exclude: /node_modules/,
+        // },
+        {
+          test: /\.ts$/,
+          use: ['ts-loader'],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  };
 }
 
 module.exports = buildConfig;
